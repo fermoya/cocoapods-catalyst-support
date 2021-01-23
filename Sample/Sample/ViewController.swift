d8a226124771afcb6b08e242c076281d9302c051
fermoya <fmdr.ct@gmail.com>
2021-01-23 18:52:47 +0000

    Supporting catalyst-only pods

---
+++ Sample/Sample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sample
+//
+//  Created by Fernando Moya de Rivas on 23/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
